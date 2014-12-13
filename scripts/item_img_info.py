56c0076287d4d86bb8d16671e63487787a78d38e
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-13 17:51:21 +0900

    done (getting contour from item img)

--- scripts/item_img_info.py
+++ scripts/item_img_info.py
@@ -36,13 +36,16 @@ def get_item_img_info(url):
     img = img[:, :, ::-1].copy()
 
     # get contour
-    imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    ret, thresh = cv2.threshold(imgray, 100, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh,
-            cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    img_with_contour = img.copy()
-    cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('with contour', img_with_contour)
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgray = cv2.GaussianBlur(imgray, (9, 9), 0)
+    ret, thresh = cv2.threshold(imgray, 0, 255,
+            cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_with_contours = img.copy()
+    cv2.drawContours(img_with_contours, contours, -1, (0, 255, 0), 1)
+    # debugging
+    cv2.imshow('with contours', img_with_contours)
+    cv2.waitKey()
 
     # debugging
     cv2.imwrite('ignore/original.png', img)
@@ -54,7 +57,7 @@ if __name__ == '__main__':
     if len(sys.argv) >= 2:
         url = sys.argv[1]
     else:
-        url = 'http://www.amazon.co.jp/%E4%B8%8D%E4%BA%8C%E8%B2%BF%E6%98%93-%E3%83%A9%E3%83%83%E3%82%AF-BOOK-%E3%83%96%E3%83%A9%E3%82%A6%E3%83%B3-81398/dp/B0034G4HL0/ref=sr_1_8?s=home&ie=UTF8&qid=1418455551&sr=1-8&keywords=%E6%A3%9A'
+        url = 'http://www.amazon.co.jp/%E4%B8%8D%E4%BA%8C%E8%B2%BF%E6%98%93-%E3%83%95%E3%83%AA%E3%83%BC%E3%83%9C%E3%83%83%E3%82%AF%E3%82%B9-FBC960-%E3%82%AB%E3%83%A9%E3%83%BC%E3%83%9C%E3%83%83%E3%82%AF%E3%82%B9-57093/dp/B00163JL6E/ref=sr_1_3?s=home&ie=UTF8&qid=1418455551&sr=1-3&keywords=%E6%A3%9A'
 
     get_item_img_info(url=url)
 

bc44204b2d3da64d361abc89d3cd48ab4e053948
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-14 15:14:27 +0900

    complete url_action.php

--- public_html/url_action.php
+++ public_html/url_action.php
@@ -1,10 +1,17 @@
 <?php
+// url
 $url = $_GET['url'];
-/*$width = $_GET['width'];
-$height = $_GET['height'];
-$depth = $_GET['depth'];*/
-
-//system("python ../scripts/get_image.py $url");
-$img = sha1($url) . '.png';
-
-header("Location: index.html?img=$img&width=$width&height=$height&depth=$depth");
+// img processing
+system("(cd ../scripts; python item_info.py $url;");
+// img_nm converted to sha
+$img_nm = sha1($url) . '.png';
+// get item size
+$f = fopen('img/'.$img_nm.'_size.csv', 'r');
+$size_txt = fread($f, filesize('img/'.$img_nm.'_size.csv'));
+$pieces = explode(",", $size_txt);
+$height = floatval($pieces[0]);
+$weight = floatval($pieces[1]);
+$depth = floatval($pieces[2]);
+fclose($f);
+// redirect
+header("Location: index.html?img=$img_nm&width=$width&height=$height&depth=$depth");

f88a10c2b87bd6232a951178f57cc7defca58e86
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-14 19:41:38 +0900

    centering amazon-ar-logo

--- public_html/index.php
+++ public_html/index.php
@@ -9,14 +9,19 @@
 <?php?>
 
 <body>
-  <div class="container">
-    <form role="form" method="GET" action="url_action.php">
-      <div class="form-group">
-        <label for="amazonItemUrl">amazon item url</label>
-        <input type="url" class="form-control" id="amazonItemUrl" name="url" placeholder="Enter amazon URL">
-      </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+  <div class="all" style="margin-left:180px;margin-top:130px;">
+    <div class="container" style="padding-top:50px;">
+      <img src="/assets/img/amazon-ar-logo.png" class="img-responsive" />
+    </div>
+    <div class="container" style="padding-top:50px;">
+      <form class="form-inline" role="form" method="GET" action="url_action.php">
+        <div class="form-group">
+          <!-- <label for="amazonItemUrl">amazon item url</label> -->
+          <input type="url" class="form-control" id="amazonItemUrl" name="url" placeholder="Enter amazon URL" style="width:800px">
+          <button type="submit" class="btn btn-primary">Go</button>
+        </div>
+      </form>
+    </div>
   </div>
 </body>
 
